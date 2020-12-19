63994c5f0a90e07fe05034b50fd9b4c8aeeceaa3
leandro prates <leandro.prates@gmail.com>
2020-12-19 17:07:24 -0300

    Projeto Spring Cloud com Eureka , Zuul , e Microservicos

--- customers/src/main/java/com/example/customers/CustomersApplication.java
+++ customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -1,5 +1,6 @@
 package com.example.customers;
 
+import com.example.customers.model.Aluno;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class CustomersApplication {
 
     public static void main(String[] args) {
+
+        Aluno aluno  = new Aluno();
+        aluno.setNumero(15);
+
+        System.out.println(aluno.getNumero());
+
+
         SpringApplication.run(CustomersApplication.class, args);
     }
 
---
+++ customers/src/main/java/com/example/customers/model/Aluno.java
@@ -0,0 +1,16 @@
+package com.example.customers.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Aluno extends Pessoa {
+
+        public Aluno(){
+            super();
+        }
+
+}
